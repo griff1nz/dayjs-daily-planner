0eef33c94b37bfd321484da86d07c7b748ddc4ee
Zion Griffin <defendingmymaininbox@gmail.com>
2023-12-01 19:52:33 -0500

    Classes for time blocks now update according to the current time, clock at the top updates every second

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,15 +8,37 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+  
+  // TODO: Add code to display the current date in the header of the page.
   var today = dayjs();
   var currentDay = document.getElementById('currentDay');
-  currentDay.textContent = today.format("MMMM DD, YYYY - h:mm:ss A")
+  currentDay.textContent = today.format("dddd, MMMM DD, YYYY - h:mm:ss A")
   setInterval(function() {
     var everySecond = dayjs();
-    var updateEverySecond = everySecond.format("MMMM DD, YYYY - h:mm:ss A")
+    var updateEverySecond = everySecond.format("dddd, MMMM DD, YYYY - h:mm:ss A")
     currentDay.textContent = updateEverySecond;
   }, 200); //updates every 200 milliseconds for maximum accuracy
-  
+
+  var hourBlocks = document.querySelectorAll(".hour");
+  console.log(hourBlocks);
+  //for loop looks for the current time, sets that block's class to present, then sets class of all previous blocks to past
+  for (var i = 0; i < hourBlocks.length; i++) {
+    if (hourBlocks[i].textContent === today.format("hA")) {
+      hourBlocks[i].parentElement.classList.remove("future");
+      hourBlocks[i].parentElement.classList.add("present");
+      for (var j = 0; j < i; j++) {
+        hourBlocks[j].parentElement.classList.remove("future");
+        hourBlocks[i].parentElement.classList.add("past");
+      }
+    }
+  }
+  //Sets all blocks to past if the time is past 5PM
+  if (today.format("HH") > 17) {
+    for (var i = 0; i < hourBlocks.length; i++) {
+      hourBlocks[i].parentElement.classList.remove("future");
+      hourBlocks[i].parentElement.classList.add("past");
+    }
+  }
   
   //
   // TODO: Add code to apply the past, present, or future class to each time
@@ -29,5 +51,5 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+  
 });
